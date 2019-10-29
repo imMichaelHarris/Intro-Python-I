eb46001d31383eb9100c516cd760758cda36f377
Michael Harris <mhguitar1997@gmail.com>
2019-10-29 12:21:26 -0700

    Pull

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,7 +8,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+# YOUR CODE 
+f = open("foo.txt", "r")
+print(f)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
